518964c19f604329e728f30a3b57ec1be395b347
Ravi Nishad <97040275+ravinishadcoder@users.noreply.github.com>
2022-05-01 18:17:39 +0530

    react assignment

---
+++ components/navbar.js
@@ -0,0 +1,22 @@
+function navbar() {
+    return `<h3>
+      <a href="#">Home</a>
+  </h3>
+  <div id="options">
+      <h3>
+          <a href="jewelery.html">jewelery</a>
+      </h3>
+      <h3>
+          <a href="electronic.html">Electronic</a>
+      </h3>
+      <h3>
+          <a href="login.html">Login</a>
+      </h3>
+      <h3>
+          <a href="signup.html">Signup</a>
+      </h3>
+  </div>`;
+  }
+  
+  export {navbar};
+  
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import {navbar} from "../components/navbar";
+
+console.log(navbar)
+document.getElementById("navbar").innerHTML = navbar();
+
+import "styles/navbar.css"
